36aed72e5f072ca0906bf18f988f89c49c695050
Didar Zhexembaev <mr.slx7@mail.ru>
2024-04-11 22:05:15 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Text.RegularExpressions;
+using static UnsignedUnrealLongNumbers.UnsignedUnrealLongNumbers;
+
+var str1 = "554";
+var str2 = "1444";
+
+Console.WriteLine(Add(str1, str2));
+
+namespace UnsignedUnrealLongNumbers
+{
+    public static class UnsignedUnrealLongNumbers
+    {
+        private const string ERROR_MESSAGE = "Provide correct numbers!";
+        private const char ZEROCHAR = '0';
+        private const int BASE = 10;
+
+        public static string? Add(string str1, string str2)
+        {
+            ThrowIfArgumentsInvalid(str1, str2);
+
+            var sum = new List<char>();
+            var carry = 0;
+
+            var i = str1.Length;
+            var j = str2.Length;
+            var length = i > j ? i : j;
+
+            while (length > 0)
+            {
+                var firstNumber = 0;
+                var secondNumber = 0;
+
+                if (i > 0)
+                {
+                    firstNumber = (char)(str1[--i] - ZEROCHAR);
+                }
+
+                if (j > 0)
+                {
+                    secondNumber = (char)(str2[--j] - ZEROCHAR);
+                }
+
+                var result = firstNumber + secondNumber + carry;
+
+                sum.Add((char)(result % BASE + ZEROCHAR));
+
+                carry = result / BASE;
+
+                length--;
+            }
+
+            if (carry > 0)
+            {
+                sum.Add((char)(carry + ZEROCHAR));
+            }
+
+            sum.Reverse();
+
+            return new string(sum.ToArray());
+        }
+
+        private static void ThrowIfArgumentsInvalid(params string[] args)
+        {
+            foreach (var arg in args)
+            {
+                if (arg == null)
+                {
+                    throw new ArgumentNullException(ERROR_MESSAGE);
+                }
+
+                if (!IsNumber(arg))
+                {
+                    throw new ArgumentException(ERROR_MESSAGE);
+                }
+            }
+        }
+
+        private static bool IsNumber(string number)
+            => Regex.IsMatch(number, @"^\d+$");
+    }
+}
---
+++ tests/UnsignedUnrealLongNumbersTest.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using Xunit;
+using static UnsignedUnrealLongNumbers.UnsignedUnrealLongNumbers;
+
+namespace UnsignedUnrealLongNumbers.tests
+{
+    public class UnsignedUnrealLongNumbersTest
+    {
+        [Fact]
+        [Description("Test equal length numbers")]
+        public void EqualLengthNumbersTest()
+        {
+            Assert.Equal("22", Add("11", "11"));
+            Assert.Equal("44", Add("22", "22"));
+            Assert.Equal("198", Add("99", "99"));
+            Assert.Equal("18", Add("9", "9"));
+        }
+
+        [Fact]
+        [Description("Test different length numbers")]
+        public void DifferentLengthNumbersTest()
+        {
+            Assert.Equal("12", Add("1", "11"));
+            Assert.Equal("24", Add("2", "22"));
+            Assert.Equal("108", Add("9", "99"));
+            Assert.Equal("100", Add("9", "91"));
+        }
+
+        [Fact]
+        [Description("Test long numbers")]
+        public void LongNumbersTest()
+        {
+            var longNumber = new string(Enumerable.Repeat('9', 100).ToArray());
+            var longNumberResultArray = Enumerable.Repeat('9', 101).ToArray();
+            longNumberResultArray[0] = '1';
+            longNumberResultArray[longNumberResultArray.Length - 1] = '8';
+
+            var longNumberResult = new string(longNumberResultArray);
+
+            Assert.Equal(longNumberResult, Add(longNumber, longNumber));
+
+            longNumber = new string(Enumerable.Repeat('1', 100).ToArray());
+            longNumberResult = new string(Enumerable.Repeat('2', 100).ToArray());
+
+            Assert.Equal(longNumberResult, Add(longNumber, longNumber));
+        }
+    }
+}
